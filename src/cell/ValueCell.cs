9dc5174b8f6750ab281aaceb51240e86ed6e2b93
James Petry <gavilancomun@gmail.com>
2015-05-20 22:28:49 +0100

    struct

--- src/cell/ValueCell.cs
+++ src/cell/ValueCell.cs
@@ -21,7 +21,7 @@ namespace kakuro.cell {
       return values.Select(v => "     " + v + "    ").ToList()[0];
     }
     else {
-      return Enumerable.Range(1, 9).Aggregate(" ", (acc, v) => acc + v);
+      return Enumerable.Range(1, 9).Aggregate(" ", (acc, v) => acc + (isPossible(v) ? v.ToString() : "."));
     }
   }
 
