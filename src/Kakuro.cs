bea137a96e45f6527e391990e142598afa7a79f3
unknown <dev@null>
2017-03-02 16:19:39 +0000

    refactor

--- src/Kakuro.cs
+++ src/Kakuro.cs
@@ -4,128 +4,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace kakuro
-{
-    class Kakuro
-    {
-        public static ValueCell v(List<int> values)
-        {
-            return new ValueCell(values);
-        }
-
-        public static ValueCell v()
-        {
-            System.Diagnostics.Debug.WriteLine("v");
-            return v(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        }
-
-        public static ValueCell v(params int[] values)
-        {
-            return v(values.ToList());
-        }
-
-
-        public static EmptyCell e()
-        {
-            return new EmptyCell();
-        }
-
-        public static DownCell d(int d)
-        {
-            return new DownCell(d);
-        }
-
-        public static AcrossCell a(int a)
-        {
-            return new AcrossCell(a);
-        }
-
-        public static DownAcrossCell da(int d, int a)
-        {
-            return new DownAcrossCell(d, a);
-        }
-
-        public static String drawRow(List<Cell> row)
-        {
-            return row.Select(c => c.draw())
-                .Aggregate("", (acc, v) => acc + v) + "\n";
-        }
-
-
-        public static String drawGrid(List<List<Cell>> grid)
-        {
-            return grid.Select(k => drawRow(k))
-                    .Aggregate("", (acc, v) => acc + v);
-        }
-
-        public static bool allDifferent<T>(ICollection<T> nums)
-        {
-            return nums.Count == new HashSet<T>(nums).Count;
-        }
-
-
-        public static List<T> conj<T>(List<T> items, T item)
-        {
-            List<T> result = new List<T>(items);
-            result.Add(item);
-            return result;
-        }
-
-        public static List<T> concatLists<T>(List<T> a, List<T> b)
-        {
-            return a.Concat(b).ToList();
-        }
-        public static SortedSet<T> asSet<T>(params T[] values)
-        {
-            return new SortedSet<T>(values);
-        }
-
-        public static List<T> asList<T>(params T[] values)
-        {
-            return new List<T>(values);
-        }
-
-        public static List<List<T>> product<T>(List<SortedSet<T>> colls)
-        {
-            switch (colls.Count)
-            {
-                case 0:
-                    return new List<List<T>>();
-                case 1:
-                    return colls[0].Select(a => asList(a)).ToList();
-                default:
-                    ICollection<T> head = colls[0];
-                    List<SortedSet<T>> tail = colls.Skip(1).ToList();
-                    List<List<T>> tailProd = product(tail);
-                    return head.SelectMany(x => tailProd.Select(ys => concatLists(asList(x), ys)))
-                            .ToList();
-            }
-        }
-        public static List<List<int>> permuteAll(List<ValueCell> vs, int target)
-        {
-            List<SortedSet<int>> values = vs.Select(v => v.values).ToList();
-            return product(values).Where(x => target == x.Sum())
-                    .ToList();
-        }
-
-        public static List<List<T>> transpose<T>(List<List<T>> m)
-        {
-            if (0 == m.Count)
-            {
-                return new List<List<T>>();
-            }
-            else
-            {
-                return Enumerable.Range(0, m[0].Count)
-                        .Select(i => m.Select(col => col[i]).ToList())
-                        .ToList();
-            }
-        }
-
-        public static bool isPossible(ValueCell v, int n)
-        {
-            return v.Contains(n);
-        }
-
+namespace kakuro {
+  class Kakuro {
+    public static ValueCell v(List<int> values) {
+      return new ValueCell(values);
     }
+
+    public static ValueCell v() {
+      System.Diagnostics.Debug.WriteLine("v");
+      return v(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    }
+
+    public static ValueCell v(params int[] values) {
+      return v(values.ToList());
+    }
+
+
+    public static EmptyCell e() {
+      return new EmptyCell();
+    }
+
+    public static DownCell d(int d) {
+      return new DownCell(d);
+    }
+
+    public static AcrossCell a(int a) {
+      return new AcrossCell(a);
+    }
+
+    public static DownAcrossCell da(int d, int a) {
+      return new DownAcrossCell(d, a);
+    }
+
+    public static String drawRow(List<Cell> row) {
+      return row.Select(c => c.draw())
+          .Aggregate("", (acc, v) => acc + v) + "\n";
+    }
+
+
+    public static String drawGrid(List<List<Cell>> grid) {
+      return grid.Select(k => drawRow(k))
+              .Aggregate("", (acc, v) => acc + v);
+    }
+
+    public static bool allDifferent<T>(ICollection<T> nums) {
+      return nums.Count == new HashSet<T>(nums).Count;
+    }
+
+
+    public static List<T> conj<T>(List<T> items, T item) {
+      List<T> result = new List<T>(items);
+      result.Add(item);
+      return result;
+    }
+
+    public static List<T> concatLists<T>(List<T> a, List<T> b) {
+      return a.Concat(b).ToList();
+    }
+    public static SortedSet<T> asSet<T>(params T[] values) {
+      return new SortedSet<T>(values);
+    }
+
+    public static List<T> asList<T>(params T[] values) {
+      return new List<T>(values);
+    }
+
+    public static List<List<T>> product<T>(List<SortedSet<T>> colls) {
+      switch (colls.Count) {
+        case 0:
+          return new List<List<T>>();
+        case 1:
+          return colls[0].Select(a => asList(a)).ToList();
+        default:
+          ICollection<T> head = colls[0];
+          List<SortedSet<T>> tail = colls.Skip(1).ToList();
+          List<List<T>> tailProd = product(tail);
+          return head.SelectMany(x => tailProd.Select(ys => concatLists(asList(x), ys)))
+                  .ToList();
+      }
+    }
+    public static List<List<int>> permuteAll(List<ValueCell> vs, int target) {
+      List<SortedSet<int>> values = vs.Select(v => v.values).ToList();
+      return product(values).Where(x => target == x.Sum())
+              .ToList();
+    }
+
+    public static List<List<T>> transpose<T>(List<List<T>> m) {
+      if (0 == m.Count) {
+        return new List<List<T>>();
+      }
+      else {
+        return Enumerable.Range(0, m[0].Count)
+                .Select(i => m.Select(col => col[i]).ToList())
+                .ToList();
+      }
+    }
+
+    public static bool isPossible(ValueCell v, int n) {
+      return v.Contains(n);
+    }
+
+    public static IEnumerable<T> takeWhile<T>(Predicate<T> f, List<T> coll) {
+      foreach (var item in coll) {
+        if (f.Invoke(item)) {
+          yield return item;
+        }
+        else {
+          yield break;
+        }
+      }
+    }
+
+  }
 }
--- src/TestKakuro2.cs
+++ src/TestKakuro2.cs
@@ -6,106 +6,104 @@ using System.Linq;
 using System.Text;
 using static kakuro.Kakuro;
 
-namespace kakuro
-{
-    class TestKakuro2
-    {
+namespace kakuro {
+  class TestKakuro2 {
 
-        [Test]
-        public void testDrawEmpty()
-        {
-            String result = e().draw();
-            Assert.AreEqual("   -----  ", result);
-        }
-
-        [Test]
-        public void testDrawAcross()
-        {
-            String result = a(5).draw();
-            Assert.AreEqual("   --\\ 5  ", result);
-        }
-
-        [Test]
-        public void testDrawDown()
-        {
-            String result = d(4).draw();
-            Assert.AreEqual("    4\\--  ", result);
-        }
-
-        [Test]
-        public void testDrawDownAcross()
-        {
-            String result = da(3, 4).draw();
-            Assert.AreEqual("    3\\ 4  ", result);
-        }
-
-        [Test]
-        public void testDrawValues()
-        {
-            String result = v().draw();
-            Assert.AreEqual(" 123456789", result);
-            String result12 = v(1, 2).draw();
-            Assert.AreEqual(" 12.......", result12);
-        }
-
-        [Test]
-        public void testDrawRow()
-        {
-            List<Cell> line = asList<Cell>(da(3, 4), v(), v(1, 2), d(4), e(), a(5), v(4), v(1));
-            String result = drawRow(line);
-            Assert.AreEqual("    3\\ 4   123456789 12.......    4\\--     -----     --\\ 5       4         1    \n", result);
-        }
-
-        [Test]
-        public void testProduct()
-        {
-            List<SortedSet<int>> data = asList(asSet(1, 2), asSet(10), asSet(100, 200, 300));
-            List<List<int>> expected = asList(
-              asList(1, 10, 100),
-              asList(1, 10, 200),
-              asList(1, 10, 300),
-              asList(2, 10, 100),
-              asList(2, 10, 200),
-              asList(2, 10, 300));
-            Assert.AreEqual(expected, product(data));
-        }
-
-        [Test]
-        public void testPermute()
-        {
-            List<ValueCell> vs = asList(v(), v(), v());
-            List<List<int>> results = permuteAll(vs, 6);
-            Assert.AreEqual(10, results.Count);
-            List<List<int>> diff = results.Where(k => allDifferent(k))
-              .ToList();
-            Assert.AreEqual(6, diff.Count);
-        }
-
-        [Test]
-        public void testTranspose()
-        {
-            List<List<int>> ints = Enumerable.Range(0, 3)
-              .Select(i => Enumerable.Range(0, 4).ToList())
-              .ToList();
-            List<List<int>> tr = transpose(ints);
-            Assert.AreEqual(ints.Count, tr[0].Count);
-            Assert.AreEqual(ints[0].Count, tr.Count);
-        }
-
-        [Test]
-        public void testValueEquality()
-        {
-            Assert.AreEqual(v(), v());
-            Assert.AreEqual(v(1, 2), v(1, 2));
-        }
-
-       [Test]
-public void testIsPoss()
-        {
-            ValueCell vc = v(1, 2, 3);
-            Assert.AreEqual(true, isPossible(vc, 2));
-            Assert.AreEqual(false, isPossible(vc, 4));
-        }
-
+    [Test]
+    public void testDrawEmpty() {
+      var result = e().draw();
+      Assert.AreEqual("   -----  ", result);
     }
+
+    [Test]
+    public void testDrawAcross() {
+      var result = a(5).draw();
+      Assert.AreEqual("   --\\ 5  ", result);
+    }
+
+    [Test]
+    public void testDrawDown() {
+      var result = d(4).draw();
+      Assert.AreEqual("    4\\--  ", result);
+    }
+
+    [Test]
+    public void testDrawDownAcross() {
+      var result = da(3, 4).draw();
+      Assert.AreEqual("    3\\ 4  ", result);
+    }
+
+    [Test]
+    public void testDrawValues() {
+      var result = v().draw();
+      Assert.AreEqual(" 123456789", result);
+      var result12 = v(1, 2).draw();
+      Assert.AreEqual(" 12.......", result12);
+    }
+
+    [Test]
+    public void testDrawRow() {
+      var line = asList<Cell>(da(3, 4), v(), v(1, 2), d(4), e(), a(5), v(4), v(1));
+      String result = drawRow(line);
+      Assert.AreEqual("    3\\ 4   123456789 12.......    4\\--     -----     --\\ 5       4         1    \n", result);
+    }
+
+    [Test]
+    public void testProduct() {
+      var data = asList(asSet(1, 2), asSet(10), asSet(100, 200, 300));
+      var expected = asList(
+        asList(1, 10, 100),
+        asList(1, 10, 200),
+        asList(1, 10, 300),
+        asList(2, 10, 100),
+        asList(2, 10, 200),
+        asList(2, 10, 300));
+      Assert.AreEqual(expected, product(data));
+    }
+
+    [Test]
+    public void testPermute() {
+      var vs = asList(v(), v(), v());
+      var results = permuteAll(vs, 6);
+      Assert.AreEqual(10, results.Count);
+      var diff = results.Where(k => allDifferent(k)).ToList();
+      Assert.AreEqual(6, diff.Count);
+    }
+
+    [Test]
+    public void testTranspose() {
+      var ints = Enumerable.Range(0, 3)
+        .Select(i => Enumerable.Range(0, 4).ToList())
+        .ToList();
+      var tr = transpose(ints);
+      Assert.AreEqual(ints.Count, tr[0].Count);
+      Assert.AreEqual(ints[0].Count, tr.Count);
+    }
+
+    [Test]
+    public void testValueEquality() {
+      Assert.AreEqual(v(), v());
+      Assert.AreEqual(v(1, 2), v(1, 2));
+    }
+
+    [Test]
+    public void testIsPoss() {
+      var vc = v(1, 2, 3);
+      Assert.AreEqual(true, isPossible(vc, 2));
+      Assert.AreEqual(false, isPossible(vc, 4));
+    }
+
+    [Test]
+    public void testTakeWhile() {
+      var result = takeWhile(n => n < 4, Enumerable.Range(0, 10).ToList()).ToList();
+      Assert.AreEqual(4, result.Count);
+    }
+
+    [Test]
+    public void testTakeWhile2() {
+      var result = takeWhile(n => (n < 4) || (n > 6), Enumerable.Range(0, 10).ToList()).ToList();
+      Assert.AreEqual(4, result.Count);
+    }
+
+  }
 }
