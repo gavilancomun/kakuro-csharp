43a5f4d38684819bc690acc9dc4af1f331443ee3
unknown <dev@null>
2017-03-04 15:38:51 +0000

    test

--- src/TestCS.cs
+++ src/TestCS.cs
@@ -5,6 +5,38 @@ using System.Linq;
 using System.Text;
 
 namespace Kakuro {
+  class Other {
+    public static bool Even(int n) {
+      return 0 == n % 2;
+    }
+    public bool MemberEven(int n) {
+      return 0 == n % 2;
+    }
+
+  }
   class TestCS {
+
+    public bool Even(int n) {
+      return 0 == n % 2;
+    }
+
+    [Test]
+    public void TestMemberDelegate() {
+      var result = Enumerable.Range(0, 10).Where(Even).ToList();
+      Assert.AreEqual(5, result.Count);
+    }
+
+    [Test]
+    public void TestRemoteStaticDelegate() {
+      var result = Enumerable.Range(0, 10).Where(Other.Even).ToList();
+      Assert.AreEqual(5, result.Count);
+    }
+
+    [Test]
+    public void TestRemoteMemberDelegate() {
+      var other = new Other();
+      var result = Enumerable.Range(0, 10).Where(other.MemberEven).ToList();
+      Assert.AreEqual(5, result.Count);
+    }
   }
 }
